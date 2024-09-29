c65864fa43e8662a6bf41f16cbfbb4bb2aa836e6
Ajay Raj <ajaygupta9210p@gmail.com>
2024-09-29 12:17:07 +0530

    some changes

---
+++ client/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+   // require('@tailwindcss/line-clamp'),
+    // ...
+  ],
+};
+// echo "# mern-estate-project" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/ajayraj1122/mern-estate-project.git
+// git push -u origin main
